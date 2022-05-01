f0c4e4104a49e22102ec5cca62af2adbf988abd7
Denes <jose.araujo@zup.com.br>
2022-05-01 17:39:56 -0300

    commit inicial

---
+++ src/main/java/br/com/zup/edu/reclamacao/ReclamacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.reclamacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReclamacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReclamacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/edu/reclamacao/ReclamacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.reclamacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReclamacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
